593c5feaa843239a34dc8b9827db0a21b5615679
Andrew Dron <u1027713@utah.edu>
2025-08-06 12:29:48 -0600

    Change fuzzing script and generic input file

--- webserver/fuzzing/scripts/fuzz_program.sh
+++ webserver/fuzzing/scripts/fuzz_program.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 cd fuzzing/scripts
 echo "Configuring AFL++ for current system"
-#sudo afl-system-config
 
+#Either change core dump pattern or declare that you don't care about missing crashes
+#export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
+#echo core | sudo tee /proc/sys/kernel/core_pattern
 
 #echo "Setting up intput corpus"
-
 #setup inputs corpus
-export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
 #afl-cmin -i ../seeds -o ../INPUTS/INPUTS_UNIQUE -- ../PLC_harness/plc_harness @@
 
 
-#rather optional scirpt to minimize input files for AFL++
-# mkdir input
-# cd INPUTS_UNIQUE
-# for i in *; do
-#   afl-tmin -i "$i" -o "../input/$i" -- ../core/openplc -someopt @@
-# done
-
 echo "Begin Fuzzing"
-afl-fuzz -i ../seeds -o ../fuzz_output -- ../PLC_Harness/plc_harness @@
+afl-fuzz -i ../INPUTS -o ../fuzz_output -- ../PLC_Harness/plc_harness @@
